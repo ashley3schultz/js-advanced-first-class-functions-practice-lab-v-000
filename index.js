a64efa86e12cd2f7131ab532054d0cb28165be8e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-28 14:35:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logDriversByHometown(drivers, town) {
 }
 
 function driversByRevenue(drivers) {
-  const newarr = drivers.slice()
+  const newarr = drivers.slice(0)
   newarr.sort(function(a, b) {
     return a.revenue - b.revenue
   })
